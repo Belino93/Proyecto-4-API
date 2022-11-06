772af43adf44a32cc38b01da28d39d9f8ac12b54
belino93 <abelmadrid93@gmail.com>
2022-11-06 17:00:06 +0100

    Update config

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ app.use(router)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 
-    //con sync({force: true}) se sincroniza sequelize con nuestra DB, force hace que pueda sobreescribir tablas
-    //con authenticate no sobreescribe y es más ligero, pero también se sincroniza
-
     // sequelize.sync({force: true}).then(() =>{
     //     console.log('Conected to database')
     // }).catch(error => console.log(`Conection error ${error}`))
